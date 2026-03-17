071c22231f01362c48e09d442921951015a784d9
sinnersanjay <sinnersanjay_17@yahoo.com>
2026-03-17 11:28:49 +0530

    updated opensslcrypto.cpp

--- src/common/Cryptography/OpenSSLCrypto.cpp
+++ src/common/Cryptography/OpenSSLCrypto.cpp
@@ -17,55 +17,20 @@
 
 #include <OpenSSLCrypto.h>
 #include <openssl/crypto.h>
-#include <vector>
-#include <thread>
-#include <mutex>
-
-std::vector<std::mutex*> cryptoLocks;
+#include <openssl/opensslv.h>
 void ValgrindRandomSetup();
-
-static void lockingCallback(int mode, int type, char const* /*file*/, int /*line*/)
-{
-    if (mode & CRYPTO_LOCK)
-        cryptoLocks[type]->lock();
-    else
-        cryptoLocks[type]->unlock();
-}
-
-static void threadIdCallback(CRYPTO_THREADID * id)
-{
-    (void)id;
-    CRYPTO_THREADID_set_numeric(id, std::hash<std::thread::id>()(std::this_thread::get_id()));
-}
 
 void OpenSSLCrypto::threadsSetup()
 {
 #ifdef VALGRIND
     ValgrindRandomSetup();
 #endif
-
-    cryptoLocks.resize(CRYPTO_num_locks());
-    for(int i = 0 ; i < CRYPTO_num_locks(); ++i)
-    {
-        cryptoLocks[i] = new std::mutex();
-    }
-
-    (void)&threadIdCallback;
-    CRYPTO_THREADID_set_callback(threadIdCallback);
-
-    (void)&lockingCallback;
-    CRYPTO_set_locking_callback(lockingCallback);
+    // OpenSSL 1.1.0+ handles threading internally; no setup required.
 }
 
 void OpenSSLCrypto::threadsCleanup()
 {
-    CRYPTO_set_locking_callback(nullptr);
-    CRYPTO_THREADID_set_callback(nullptr);
-    for(int i = 0 ; i < CRYPTO_num_locks(); ++i)
-    {
-        delete cryptoLocks[i];
-    }
-    cryptoLocks.resize(0);
+    // OpenSSL 1.1.0+ handles threading internally; no cleanup required.
 }
 
 #ifdef VALGRIND
